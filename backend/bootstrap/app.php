d1f61ada23762b50b883bd8f7be8754faa1ff825
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-02-09 20:24:51 +0100

    Register Middlewares

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Middleware\IsAdmin;
+use App\Http\Middleware\IsStudent;
+use App\Http\Middleware\IsTeacher;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -7,11 +10,16 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'is_admin' => IsAdmin::class,
+            'is_teacher' => IsTeacher::class,
+            'is_student' => IsStudent::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
