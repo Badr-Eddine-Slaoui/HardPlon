5753c1fd89639a9e857d856cd47b5059f1712a55
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-02-09 20:22:39 +0100

    Add Class Group Type

---
+++ frontend/types/class_group.ts
@@ -0,0 +1,25 @@
+import type { Formation } from "./formation"
+import type { ClassTeacher, User } from "./user"
+
+export interface ClassGroup {
+    id: number
+    formation_id: number
+    name: string
+    image_url: string
+    capacity: number
+    description: string
+    created_at: string
+    updated_at: string
+    deleted_at: string | null
+    students_count: number
+    is_active: boolean
+    formation: Formation
+    students: User[]
+    main_teacher: ClassTeacher
+    sub_teacher: ClassTeacher
+}
+
+export interface ClassGroupData {
+    class_groups: ClassGroup[]
+    archived_class_groups: ClassGroup[]
+}
