d35b61e4a17395543e70f9d6f49115227eadfbd4
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-02-09 20:23:58 +0100

    Add Submitting Type

---
+++ frontend/types/submitting.ts
@@ -0,0 +1,17 @@
+import type { User } from './user';
+import type { Brief } from './brief';
+
+export interface Submitting {
+    id: number;
+    brief_id: number;
+    student_id: number;
+    squad_id: number | null;
+    message: string;
+    links: { [key: string]: string}[];
+    created_at: string;
+    updated_at: string;
+    deleted_at: string | null;
+    student: User;
+    squad: any;
+    brief: Brief;
+}
