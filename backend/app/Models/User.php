de606c3e912617d038f377d42337745364241eb5
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-02-09 23:22:02 +0100

    Add Role To JWT

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -45,7 +45,9 @@ class User extends Authenticatable implements JWTSubject
 
     public function getJWTCustomClaims()
     {
-        return [];
+        return [
+            'role' => $this->role,
+        ];
     }
 
     public function getIsActiveAttribute(){
