c0a3324011f433f80c250fd0a22771d19a6f0392
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-02-09 20:05:49 +0100

    Add QuillEditor Client Component

---
+++ frontend/app/components/QuillEditor.client.vue
@@ -0,0 +1,26 @@
+<template>
+    <QuillEditor
+        :content="modelValue"
+        @update:content="onUpdate"
+        theme="snow"
+        toolbar="full"
+        content-type="html"
+    />
+</template>
+
+<script setup lang="ts">
+    import { QuillEditor } from '@vueup/vue-quill'
+    import '@vueup/vue-quill/dist/vue-quill.snow.css'
+
+    const props = defineProps<{
+        modelValue: string
+    }>()
+
+    const emit = defineEmits<{
+        (e: 'update:modelValue', value: string): void
+    }>()
+
+    const onUpdate = (value: string) => {
+        emit('update:modelValue', value)
+    }
+</script>
