15c4a1aa28b2e49dc250eeee300c730220a4ceb9
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-02-09 20:23:28 +0100

    Add Skill Type

---
+++ frontend/types/skill.ts
@@ -0,0 +1,31 @@
+import type { Formation } from './formation';
+import type { Level } from './level';
+
+export interface SkillLevel {
+    skill_id: number;
+    level_id: number;
+    criteria: string;
+    created_at: string | null;
+    updated_at: string | null;
+    deleted_at: string | null;
+    level: Level;
+}
+
+export interface Skill {
+    id: number;
+    formation_id: number;
+    code: string;
+    title: string;
+    description: string;
+    created_at: string;
+    updated_at: string;
+    deleted_at: string | null;
+    is_active: boolean;
+    formation?: Formation;
+    skill_levels?: SkillLevel[];
+}
+
+export interface SkillData {
+    skills: Skill[]
+    archived_skills: Skill[]
+}
