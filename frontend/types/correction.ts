2e14c89f057144319071acbe97b96b85a0f944ba
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-02-09 20:22:48 +0100

    Add Correction Type

---
+++ frontend/types/correction.ts
@@ -0,0 +1,28 @@
+import type { BriefSkillLevel, Brief } from './brief';
+import type { User } from './user';
+
+export interface CorrectionDetail {
+    id: number;
+    correction_id: number;
+    brief_skill_level_id: number;
+    grade: string;
+    created_at: string;
+    updated_at: string;
+    deleted_at: string | null;
+    brief_skill_level: BriefSkillLevel
+}
+
+export interface Correction {
+    id: number;
+    brief_id: number;
+    student_id: number;
+    teacher_id: number;
+    message: string;
+    created_at: string;
+    updated_at: string;
+    deleted_at: string | null;
+    brief: Brief;
+    teacher: User;
+    student: User;
+    correction_details: CorrectionDetail[];
+}
