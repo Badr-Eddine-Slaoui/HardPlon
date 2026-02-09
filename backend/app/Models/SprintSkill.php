12518514b2ff18b7572ff67d1b8db0f4e92779dc
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-02-09 09:23:13 +0100

    Add Sprint Skill Model

--- backend/app/Models/SprintSkill.php
+++ backend/app/Models/SprintSkill.php
@@ -9,9 +9,19 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class SprintSkill extends Model
 {
     use HasFactory, SoftDeletes;
+    public $incrementing = false;
+    protected $primaryKey = null;
     protected $table = "sprint_skills";
     protected $fillable = [
         'sprint_id',
         'skill_id',
     ];
+
+    public function sprint(){
+        return $this->belongsTo(Sprint::class);
+    }
+
+    public function skill(){
+        return $this->belongsTo(Skill::class);
+    }
 }
