aeaef8ea082bbeb316f5330087fe853410fa3a3c
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-02-19 14:13:05 +0000

    Add A Default Admin In User Seeder

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +14,17 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory()->create([
+            'first_name' => "Badr Eddine",
+            'last_name' => "Slaoui",
+            'age' => 22,
+            'email' => "slaoui.badr.eddine.sdv@gmail.com",
+            'cin' => "HH127747",
+            'phone' => "0713387592",
+            'password' => Hash::make('1811977'),
+            'role' => "ADMIN",
+            'id_class' => null,
+        ]);
         User::factory(150)->create();
     }
 }
