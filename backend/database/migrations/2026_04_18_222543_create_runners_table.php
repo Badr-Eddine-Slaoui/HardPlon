dd19eba764a25ca0ca8a5f2aade67fb3e9716eb0
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-04-22 00:34:29 +0100

    feat: Create migration for runners table with attributes and relationships

---
+++ backend/database/migrations/2026_04_18_222543_create_runners_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('runners', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('type', ['http', 'browser', 'function']);
+            $table->text('description')->nullable();
+            $table->enum('status', ["active", "deprecated", "disabled"])->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('runners');
+    }
+};
