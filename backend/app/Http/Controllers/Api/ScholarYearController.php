9f66f892abe089d563149d0ff7f36729703e8656
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-04-22 14:23:37 +0100

    fix: Enhance error handling and standardize responses in ScholarYearController

--- backend/app/Http/Controllers/Api/ScholarYearController.php
+++ backend/app/Http/Controllers/Api/ScholarYearController.php
@@ -6,112 +6,220 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\ScholarYearRequest;
 use App\Models\ScholarYear;
 use Illuminate\Http\Request;
+use Laravel\Octane\Facades\Octane;
 
 class ScholarYearController extends Controller
 {
     public function index()
     {
-        $scholar_years = ScholarYear::withTrashed()->with(['grade_levels'])->orderByDesc('created_at')->get();
-        $archived_years = ScholarYear::onlyTrashed()->get();
-        $actived_years = ScholarYear::withoutTrashed()->get();
-        $current_year = $actived_years->filter(function ($year) {
-            return $year->is_current();
-        })->first();
 
-        return response()->json([
-            "success" => true,
-            "data" => compact("scholar_years", "archived_years", "current_year")
-        ], 200);
+        try {
+            $per_page = request()->get("per_page", 5);
+
+            [$scholar_years, $archived_years, $actived_years, $current_year] = Octane::concurrently([
+                fn() => ScholarYear::withoutTrashed()->with(['grade_levels'])->orderByDesc('created_at')->paginate($per_page),
+                fn() => ScholarYear::onlyTrashed()->with(['grade_levels'])->orderByDesc('created_at')->paginate($per_page),
+                fn() => ScholarYear::withoutTrashed()->orderByDesc('created_at')->get(),
+                fn() => ScholarYear::withoutTrashed()->get()->filter(function ($year) {
+                    return $year->is_current();
+                })->first(),
+            ]);
+
+            return response()->json([
+                "success" => true,
+                "data" => compact("scholar_years", "archived_years", "current_year", "actived_years"),
+                "message" => "Successfully fetched scholar years."
+            ], 200);
+
+        } catch (\Throwable $e) {
+            return response()->json([
+                "success" => false,
+                "data" => null,
+                "message" => "Something went wrong. Please try again.",
+                "code" => $e->getCode()
+            ], 500);
+        }
     }
 
-    public function show(Request $request, ScholarYear $scholar_year){
+    public function show(Request $request, int $id){
 
-        $scholar_year->loadMissing('grade_levels');
+        try {
+            $scholar_year = ScholarYear::with(['grade_levels'])->withTrashed()->find($id);
 
-        return response()->json([
-            "success" => true,
-            "data" => compact("scholar_year")
-        ], 200);
+            if (!$scholar_year) {
+                return response()->json([
+                    "success" => false,
+                    "data" => null,
+                    "message" => "Scholar year not found."
+                ], 404);
+            }
+
+            return response()->json([
+                "success" => true,
+                "data" => compact("scholar_year"),
+                "message" => "Successfully fetched scholar year."
+            ], 200);
+
+        } catch (\Throwable $e) {
+            return response()->json([
+                "success" => false,
+                "data" => null,
+                "message" => "Something went wrong. Please try again.",
+                "code" => $e->getCode()
+            ], 500);
+        }
     }
 
     public function store(ScholarYearRequest $request)
     {
-        $scholar_year = ScholarYear::create([
-            "start_year" => $request->start_year,
-            "end_year" => $request->end_year,
-            "capacity" => $request->capacity
-        ]);
+        try {
+            $scholar_year = ScholarYear::create([
+                "start_year" => $request->start_year,
+                "end_year" => $request->end_year,
+                "capacity" => $request->capacity
+            ]);
 
-        if ($scholar_year) {
+            if ($scholar_year) {
+                return response()->json([
+                    "success" => true,
+                    "data" => compact("scholar_year"),
+                    "message" => "Successfully created scholar year."
+                ], 200);
+            }
+
             return response()->json([
-                "success" => true,
-                "data" => compact("scholar_year"),
-                "message" => "Successfully created scholar year."
-            ], 200);
+                "success" => false,
+                "data" => null,
+                "message" => "Something went wrong. Please try again."
+            ], 400);
+        } catch (\Throwable $e) {
+            return response()->json([
+                "success" => false,
+                "data" => null,
+                "message" => "Something went wrong. Please try again.",
+                "code" => $e->getCode()
+            ], 500);
         }
-
-        return response()->json([
-            "success" => false,
-            "message" => "Something went wrong. Please try again."
-        ], 500);
     }
 
-    public function update(ScholarYearRequest $request, ScholarYear $scholar_year)
+    public function update(ScholarYearRequest $request, int $id)
     {
-        $is_updated = $scholar_year->update([
-            "start_year" => $request->start_year,
-            "end_year" => $request->end_year,
-            "capacity" => $request->capacity
-        ]);
+        try {
+            $scholar_year = ScholarYear::withTrashed()->find($id);
 
-        if ($is_updated) {
+            if (!$scholar_year) {
+                return response()->json([
+                    "success" => false,
+                    "data" => null,
+                    "message" => "Scholar year not found."
+                ], 404);
+            }
+
+            $is_updated = $scholar_year->update([
+                "start_year" => $request->start_year,
+                "end_year" => $request->end_year,
+                "capacity" => $request->capacity
+            ]);
+
+            if ($is_updated) {
+                return response()->json([
+                    "success" => true,
+                    "data" => compact("scholar_year"),
+                    "message" => "Successfully updated scholar year."
+                ], 200);
+            }
+
             return response()->json([
-                "success" => true,
-                "data" => compact("scholar_year"),
-                "message" => "Successfully updated scholar year."
-            ], 200);
-        }
+                "success"=> false,
+                "data" => null,
+                "message" => "Something went wrong. Please try again."
+            ], 400);
 
-        return response()->json([
-            "success"=> false,
-            "message" => "Something went wrong. Please try again."
-        ], 500);
+        } catch (\Throwable $e) {
+            return response()->json([
+                "success" => false,
+                "data" => null,
+                "message" => "Something went wrong. Please try again.",
+                "code" => $e->getCode()
+            ], 500);
+        }
     }
 
-    public function destroy(ScholarYear $scholar_year)
+    public function destroy(int $id)
     {
-        $is_deleted = $scholar_year->delete();
+        try {
+            $scholar_year = ScholarYear::withoutTrashed()->find($id);
 
-        if ($is_deleted) {
+            if (!$scholar_year) {
+                return response()->json([
+                    "success" => false,
+                    "data" => null,
+                    "message" => "Scholar year not found."
+                ], 404);
+            }
+
+            $is_deleted = $scholar_year->delete();
+
+            if ($is_deleted) {
+                return response()->json([
+                    "success" => true,
+                    "data" => compact("scholar_year"),
+                    "message" => "Successfully deleted scholar year."
+                ], 200);
+            }
+
             return response()->json([
-                "success" => true,
-                "data" => compact("scholar_year"),
-                "message" => "Successfully deleted scholar year."
-            ], 200);
-        }
+                "success"=> false,
+                "data" => null,
+                "message" => "Something went wrong. Please try again."
+            ], 400);
 
-        return response()->json([
-            "success"=> false,
-            "message" => "Something went wrong. Please try again."
-        ], 500);
+        } catch (\Throwable $e) {
+            return response()->json([
+                "success" => false,
+                "data" => null,
+                "message" => "Something went wrong. Please try again.",
+                "code" => $e->getCode()
+            ], 500);
+        }
     }
 
     public function restore(int $id)
     {
-        $scholar_year = ScholarYear::withTrashed()->find($id);
-        $is_restored = $scholar_year->restore();
+        try {
+            $scholar_year = ScholarYear::onlyTrashed()->find($id);
 
-        if ($is_restored) {
+            if (!$scholar_year) {
+                return response()->json([
+                    "success" => false,
+                    "data" => null,
+                    "message" => "Scholar year not found."
+                ], 404);
+            }
+
+            $is_restored = $scholar_year->restore();
+
+            if ($is_restored) {
+                return response()->json([
+                    "success" => true,
+                    "data" => compact("scholar_year"),
+                    "message" => "Successfully restored scholar year."
+                ], 200);
+            }
+
             return response()->json([
-                "success" => true,
-                "data" => compact("scholar_year"),
-                "message" => "Successfully restored scholar year."
-            ], 200);
-        }
+                "success"=> false,
+                "data" => null,
+                "message" => "Something went wrong. Please try again."
+            ], 400);
 
-        return response()->json([
-            "success"=> false,
-            "message" => "Something went wrong. Please try again."
-        ], 500);
+        } catch (\Throwable $e) {
+            return response()->json([
+                "success" => false,
+                "data" => null,
+                "message" => "Something went wrong. Please try again.",
+                "code" => $e->getCode()
+            ], 500);
+        }
     }
 }
