685f8ae8e111a893234e69740e46eddb24e83ac9
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-04-23 20:34:03 +0100

    feat: Update ReturnData interface and enhance AuthResponse structure

--- frontend/types/api.ts
+++ frontend/types/api.ts
@@ -2,11 +2,24 @@ import type { User } from "./user";
 
 export interface AuthResponse {
     access_token: string;
+    token_type: string;
     user: User;
 }
 
-export interface ReturnData<T>{
+export interface ReturnData<T = null>{
     success: boolean;
-    data?: T;
+    data: T | null;
+    message: string;
     errors?: any;
+}
+
+export interface meta {
+    current_page: number;
+    last_page: number;
+    next_page_url: string | null;
+    prev_page_url: string | null;
+    total: number;
+    per_page: number;
+    from: number;
+    to: number;
 }
