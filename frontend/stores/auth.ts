0fb53a5841d95901dd4a5ebd39c8ecdc2219b8f5
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-04-23 20:32:52 +0100

    feat: Enhance login and logout functions with error handling and toast notifications

--- frontend/stores/auth.ts
+++ frontend/stores/auth.ts
@@ -1,6 +1,9 @@
 import { defineStore } from 'pinia';
 import { ref, computed } from 'vue';
 import type { AuthResponse, ReturnData } from '~~/types/api';
+import { api } from '../app/utils/api';
+import type { User } from '~~/types/user';
+import { useToastStore } from './toast';
 
 
 
@@ -10,28 +13,47 @@ export const useAuthStore = defineStore(
         const user = ref<any>(null)
         const token = ref<string | null>(null)
 
+        const toast = useToastStore()
+
         const isLoggedIn = computed(() => !!token.value)
 
         async function login(data: { email: string; password: string }): Promise<ReturnData<any>> {
             try {
-                const res = await api<AuthResponse>('/login', {
+                const res = await api<ReturnData<AuthResponse>>('/login', {
                     method: 'POST',
                     body: data,
                 })
-                token.value = res.access_token
-                user.value = res.user
+
+                token.value = res.data?.access_token as string
+                user.value = res.data?.user as User
+
+                toast.push({
+                    message: 'Login successful',
+                    type: 'success',
+                })
+
                 return {
                     success: true,
                     data: token.value,
+                    message: res.message,
                 }
+
             } catch (err: any) {
                 if (err?.data?.errors) {
                     if (err.data.errors.email || err.data.errors.password) {
                         let { email, password } = err.data.errors;
                         email = email ? email[0] : "";
                         password = password ? password[0] : "";
+
+                        toast.push({
+                            message: 'Validation errors',
+                            type: 'error',
+                        })
+
                         return {
                             success: false,
+                            data: null,
+                            message: 'Validation errors',
                             errors: {
                                 email,
                                 password,
@@ -42,8 +64,16 @@ export const useAuthStore = defineStore(
                 }
 
                 if(err?.data?.message) {
+
+                    toast.push({
+                        message: err.data.message,
+                        type: 'error',
+                    })
+
                     return {
                         success: false,
+                        data: null,
+                        message: err.data.message,
                         errors: {
                             email: '',
                             password: '',
@@ -51,22 +81,48 @@ export const useAuthStore = defineStore(
                         },
                     }
                 }
+                
+                toast.push({
+                    message: 'Something went wrong. Please try again.',
+                    type: 'error',
+                })
 
+                
                 return {
                     success: false,
+                    data: null,
+                    message: 'Something went wrong. Please try again.',
                     errors: null,
                 }
             }
         }
 
         async function fetchUser() {
-            user.value = await api<any>('/me')
+            try {
+                user.value = (await api<ReturnData<{ user: User }>>('/me')).data?.user
+            } catch (err) {
+                toast.push({
+                    message: 'Something went wrong. Please try again.',
+                    type: 'error',
+                })
+            }
         }
 
         async function logout() {
-            await api<any>('/logout', { method: 'POST' })
-            token.value = null
-            user.value = null
+            try {
+                const res = await api<ReturnData>('/logout', { method: 'POST' })
+                token.value = null
+                user.value = null
+                toast.push({
+                    message: res.message,
+                    type: 'success',
+                })
+            } catch (err) {
+                toast.push({
+                    message: 'Something went wrong. Please try again.',
+                    type: 'error',
+                })
+            }
         }
 
         const decoded = computed(() =>
