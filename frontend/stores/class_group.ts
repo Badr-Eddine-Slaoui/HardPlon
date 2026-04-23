f2618f3f7ae947d3fd34b6580227a1cd26d517be
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-04-23 20:33:04 +0100

    feat: Enhance class group management with error handling and toast notifications

--- frontend/stores/class_group.ts
+++ frontend/stores/class_group.ts
@@ -1,8 +1,10 @@
 import { defineStore } from 'pinia';
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 import { api } from '~/utils/api';
-import type { ReturnData } from '../types/api';
-import type { ClassGroup, ClassGroupData } from '../types/class_group';
+import type { meta, ReturnData } from '../types/api';
+import type { ClassGroup } from '../types/class_group';
+import { useToastStore } from './toast';
+import { PaginatedData } from '../types/pagination';
 
 export const useClassGroup = defineStore(
     'class_group',
@@ -10,33 +12,79 @@ export const useClassGroup = defineStore(
         const class_groups = ref<ClassGroup[] | null>(null)
         const class_group = ref<ClassGroup | null>(null)
         const archived_class_groups = ref<ClassGroup[] | null>(null)
+        const toast = useToastStore()
+        const meta: meta = reactive({
+            current_page: 0,
+            last_page: 0,
+            next_page_url: null,
+            prev_page_url: null,
+            total: 0,
+            per_page: 0,
+            from: 0,
+            to: 0
+        })
 
-        async function fetchClassGroups(): Promise<void> {
-            const res = await api<ReturnData<ClassGroupData>>('/admin/class-groups')
-            class_groups.value = res.data?.class_groups as ClassGroup[]
-            archived_class_groups.value = res.data?.archived_class_groups as ClassGroup[]
+        async function fetchClassGroups(page: number = 1, per_page: number = 5): Promise<void> {
+            try {
+                const res = await api<ReturnData<{ class_groups: PaginatedData<ClassGroup[]>, archived_class_groups: PaginatedData<ClassGroup[]> }>>(`/admin/class-groups?page=${page}&per_page=${per_page}`)
+                class_groups.value = res.data?.class_groups?.data as ClassGroup[]
+                archived_class_groups.value = res.data?.archived_class_groups?.data as ClassGroup[]
+                Object.assign(meta, res.data?.class_groups)
+            } catch (err) {
+                toast.push({
+                    message: 'Something went wrong. Please try again.',
+                    type: 'error',
+                })
+            }
         }
 
         async function fetchClassGroup(id: number): Promise<void> {
-            const res = await api<ReturnData<{ class_group: ClassGroup}>>(`/admin/class-groups/${id}`)
-            class_group.value = res.data?.class_group as ClassGroup
+            try {
+                const res = await api<ReturnData<{ class_group: ClassGroup}>>(`/admin/class-groups/${id}`)
+                class_group.value = res.data?.class_group as ClassGroup
+            } catch (err) {
+                toast.push({
+                    message: 'Something went wrong. Please try again.',
+                    type: 'error',
+                })
+            }
         }
 
         async function fetchTeacherClassGroups(): Promise<ClassGroup[]> {
-            const res = await api<ReturnData<ClassGroupData>>(`/teacher/class-groups`)
-            return res.data?.class_groups as ClassGroup[]
+            try {
+                const res = await api<ReturnData<{ class_groups: ClassGroup[] }>>('/teacher/class-groups')
+                return res.data?.class_groups as ClassGroup[]
+            } catch (err) {
+                toast.push({
+                    message: 'Something went wrong. Please try again.',
+                    type: 'error',
+                })
+                return []
+            }
         }
 
-        async function createClassGroup(data: { formation_id: number, name: string, description: string, capacity: number, image_url: string, main_teacher_id: number, sub_teacher_id: number, students_ids: number[] }): Promise<ReturnData<any>> {
+        async function createClassGroup(data: { formation_id: number, name: string, description: string, capacity: number, image_url: string, main_teacher_id: number, sub_teacher_id: number, students_ids: number[] }): Promise<ReturnData>{
             try{
                 const res = await api<ReturnData<{ class_group: ClassGroup, message: string}>>('/admin/class-groups', {
                     method: 'POST',
                     body: data
                 })
-                class_groups.value?.push(res.data?.class_group as ClassGroup)
+
+                if(!class_groups.value?.length){
+                    await fetchClassGroups()
+                }
+
+                class_groups.value?.unshift(res.data?.class_group as ClassGroup)
+
+                toast.push({
+                    message: res?.message || 'Class group created successfully.',
+                    type: 'success',
+                })
+
                 return {
                     success: true,
-                    data: res.data?.message
+                    data: null,
+                    message: res.message || 'Class group created successfully.'
                 }
             } catch (err: any) {
                 if (err?.data?.errors) {
@@ -50,8 +98,16 @@ export const useClassGroup = defineStore(
                         main_teacher_id = main_teacher_id ? main_teacher_id[0] : "";
                         sub_teacher_id = sub_teacher_id ? sub_teacher_id[0] : "";
                         students_ids = students_ids ? students_ids[0] : "";
+
+                        toast.push({
+                            message: "Validation error. Please check your input.",
+                            type: 'error',
+                        })
+
                         return {
                             success: false,
+                            message: "Validation error. Please check your input.",
+                            data: null,
                             errors: {
                                 formation_id,
                                 name,
@@ -65,8 +121,16 @@ export const useClassGroup = defineStore(
                         }
                     }
                 }
+
+                toast.push({
+                    message: err?.data?.message || 'Something went wrong. Please try again.',
+                    type: 'error',
+                })
+
                 return {
                     success: false,
+                    data: null,
+                    message: err?.data?.message || 'Something went wrong. Please try again.',
                     errors: {
                         formation_id: '',
                         name: '',
@@ -82,7 +146,7 @@ export const useClassGroup = defineStore(
             }
         }
 
-        async function updateClassGroup(id: number, data: { formation_id: number, name: string, description: string, capacity: number, image_url: string, main_teacher_id: number, sub_teacher_id: number, students_ids: number[] }): Promise<ReturnData<any>> {
+        async function updateClassGroup(id: number, data: { formation_id: number, name: string, description: string, capacity: number, image_url: string, main_teacher_id: number, sub_teacher_id: number, students_ids: number[] }): Promise<ReturnData> {
             try{
                 const res = await api<ReturnData<{ class_group: ClassGroup, message: string}>>(`/admin/class-groups/${id}`, {
                     method: 'PUT',
@@ -97,9 +161,16 @@ export const useClassGroup = defineStore(
                 const class_group = class_groups.value?.find(y => y.id === id) as ClassGroup
                 
                 Object.assign(class_group, restoredClassGroup)
+
+                toast.push({
+                    message: res?.message || 'Class group updated successfully.',
+                    type: 'success',
+                })
+
                 return {
                     success: true,
-                    data: res.data?.message
+                    message: res.message,
+                    data: null
                 }
             } catch (err: any) {
                 if (err?.data?.errors) {
@@ -113,8 +184,16 @@ export const useClassGroup = defineStore(
                         main_teacher_id = main_teacher_id ? main_teacher_id[0] : "";
                         sub_teacher_id = sub_teacher_id ? sub_teacher_id[0] : "";
                         students_ids = students_ids ? students_ids[0] : "";
+
+                        toast.push({
+                            message: "Validation error. Please check your input.",
+                            type: 'error',
+                        })
+
                         return {
                             success: false,
+                            message: "Validation error. Please check your input.",
+                            data: null,
                             errors: {
                                 formation_id,
                                 name,
@@ -128,8 +207,16 @@ export const useClassGroup = defineStore(
                         }
                     }
                 }
+
+                toast.push({
+                    message: err?.data?.message || 'Something went wrong. Please try again.',
+                    type: 'error',
+                })
+
                 return {
                     success: false,
+                    data: null,
+                    message: err?.data?.message || 'Something went wrong. Please try again.',
                     errors: {
                         title: '',
                         description: '',
@@ -147,33 +234,57 @@ export const useClassGroup = defineStore(
         }
 
         async function archiveClassGroup(id: number): Promise<void> {
-            const res = await api<ReturnData<{ class_group: ClassGroup }>>(`/admin/class-groups/${id}`, {
-                method: 'DELETE'
-            })
+            try {
+                const res = await api<ReturnData<{ class_group: ClassGroup }>>(`/admin/class-groups/${id}`, {
+                    method: 'DELETE'
+                })
 
-            if(!class_groups.value?.length){
-                await fetchClassGroups()
+                if(!class_groups.value?.length){
+                    await fetchClassGroups()
+                }
+
+                const restoredClassGroup = res.data?.class_group as ClassGroup
+                const class_group = class_groups.value?.find(y => y.id === id) as ClassGroup
+
+                Object.assign(class_group, restoredClassGroup)
+
+                toast.push({
+                    message: res?.message || 'Class group archived successfully.',
+                    type: 'success',
+                })
+            } catch (err) {
+                toast.push({
+                    message: 'Something went wrong. Please try again.',
+                    type: 'error',
+                })
             }
-
-            const restoredClassGroup = res.data?.class_group as ClassGroup
-            const class_group = class_groups.value?.find(y => y.id === id) as ClassGroup
-
-            Object.assign(class_group, restoredClassGroup)
         }
 
         async function restoreClassGroup(id: number): Promise<void> {                                                                                                                                                    
-            const res = await api<ReturnData<{ class_group: ClassGroup }>>(`/admin/class-groups/${id}/restore`, {
-                method: 'POST'
-            })
+            try {
+                const res = await api<ReturnData<{ class_group: ClassGroup }>>(`/admin/class-groups/${id}/restore`, {
+                    method: 'POST'
+                })
 
-            if(!class_groups.value?.length){
-                await fetchClassGroups()
-            }                                                                                                                                                                   
+                if(!class_groups.value?.length){
+                    await fetchClassGroups()
+                }                                                                                                                                                                   
 
-            const restoredClassGroup = res.data?.class_group as ClassGroup
-            const class_group = class_groups.value?.find(y => y.id === id) as ClassGroup
+                const restoredClassGroup = res.data?.class_group as ClassGroup
+                const class_group = class_groups.value?.find(y => y.id === id) as ClassGroup
 
-            Object.assign(class_group, restoredClassGroup)
+                Object.assign(class_group, restoredClassGroup)
+
+                toast.push({
+                    message: res?.message || 'Class group restored successfully.',
+                    type: 'success',
+                })
+            } catch (err) {
+                toast.push({
+                    message: 'Something went wrong. Please try again.',
+                    type: 'error',
+                })
+            }
         }
 
         return {
