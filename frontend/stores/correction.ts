6aafc76e62899e52db2a0254b1cab6c238fef3f6
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-04-23 20:33:09 +0100

    feat: Add error handling and toast notifications for correction fetching functions

--- frontend/stores/correction.ts
+++ frontend/stores/correction.ts
@@ -1,125 +1,76 @@
 import { defineStore } from 'pinia';
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 import { api } from '~/utils/api';
-import type { ReturnData } from '../types/api';
+import type { meta, ReturnData } from '../types/api';
 import type { Correction } from '../types/correction';
+import { useToastStore } from './toast';
 
 export const useCorrection = defineStore(
     'correction',
     () => {
         const corrections = ref<Correction[] | null>(null)
         const correction = ref<Correction | null>(null)
+        const toast = useToastStore()
+        const meta: meta = reactive({
+            current_page: 0,
+            last_page: 0,
+            next_page_url: null,
+            prev_page_url: null,
+            total: 0,
+            per_page: 0,
+            from: 0,
+            to: 0
+        })
 
         async function fetchStudentCorrections(): Promise<void> {
-            const res = await api<ReturnData<{ corrections: Correction[]}>>('/student/corrections')
-            corrections.value = res.data?.corrections as Correction[]
+            try {
+                const res = await api<ReturnData<{ corrections: Correction[]}>>('/student/corrections')
+                corrections.value = res.data?.corrections as Correction[]
+                Object.assign(meta, res.data?.corrections)
+            } catch (err) {
+                toast.push({
+                    message: 'Something went wrong. Please try again.',
+                    type: 'error',
+                })
+            }
         }
 
         async function fetchCorrection(brief_id: number): Promise<void> {
-            const res = await api<ReturnData<{ correction: Correction}>>(`/student/corrections/${brief_id}`)
-            correction.value = res.data?.correction as Correction
+            try {
+                const res = await api<ReturnData<{ correction: Correction}>>(`/student/corrections/${brief_id}`)
+                correction.value = res.data?.correction as Correction
+            } catch (err) {
+                toast.push({
+                    message: 'Something went wrong. Please try again.',
+                    type: 'error',
+                })
+            }
         }
 
         async function fetchStudentCorrection(brief_id: number, student_id: number): Promise<void> {
-            const res = await api<ReturnData<{ correction: Correction}>>(`/teacher/corrections/${brief_id}/${student_id}`)
-            correction.value = res.data?.correction as Correction
+            try {
+                const res = await api<ReturnData<{ correction: Correction}>>(`/teacher/corrections/${brief_id}/${student_id}`)
+                correction.value = res.data?.correction as Correction
+            } catch (err) {
+                toast.push({
+                    message: 'Something went wrong. Please try again.',
+                    type: 'error',
+                })
+            }
         }
 
         async function fetchStudentCorrectionsById(student_id: number): Promise<void> {
-            const res = await api<ReturnData<{ corrections: Correction[]}>>(`/teacher/corrections/students/${student_id}`)
-            corrections.value = res.data?.corrections as Correction[]
-        }
-
-        async function createCorrection(data: { brief_id: number, student_id: number, message: string, details: { brief_skill_level_id: number, grade: string }[] }): Promise<ReturnData<any>> {
-            try{
-                const res = await api<ReturnData<{ correction: Correction, message: string }>>(`/teacher/corrections`, {
-                    method: 'POST',
-                    body: data
+            try {
+                const res = await api<ReturnData<{ corrections: Correction[]}>>(`/teacher/corrections/students/${student_id}`)
+                corrections.value = res.data?.corrections as Correction[]
+            } catch (err) {
+                toast.push({
+                    message: 'Something went wrong. Please try again.',
+                    type: 'error',
                 })
-                corrections.value?.push(res.data?.correction as Correction)
-                return {
-                    success: true,
-                    data: res.data?.message
-                }
-            } catch (err: any) {
-                if (err?.data?.errors) {
-                    if (err.data.errors.brief_id || err.data.errors.student_id || err.data.errors.message || err.data.errors.details) {
-                        let { brief_id, student_id, message, details } = err.data.errors;
-                        brief_id = brief_id ? brief_id[0] : "";
-                        student_id = student_id ? student_id[0] : "";
-                        message = message ? message[0] : "";
-                        details = details ? details[0] : "";
-                        return {
-                            success: false,
-                            errors: {
-                                brief_id,
-                                student_id,
-                                message,
-                                details
-                            },
-                        }
-                    }
-                }
-                return {
-                    success: false,
-                    errors: {
-                        brief_id: '',
-                        student_id: '',
-                        message: '',
-                        details: ''
-                    },
-                }
             }
         }
 
-        async function updateCorrection(id: number, data: { brief_id: number, student_id: number, message: string, details: { brief_skill_level_id: number, grade: string }[] }): Promise<ReturnData<any>> {
-            try{
-                const res = await api<ReturnData<{ correction: Correction, message: string }>>(`/teacher/corrections/${id}`, {
-                    method: 'PUT',
-                    body: data
-                })
-
-                if(!corrections.value?.length){
-                    await fetchStudentCorrectionsById(data.student_id)
-                }
-
-                const correction = corrections.value?.find(y => y.id === id) as Correction
-
-                Object.assign(correction, res.data?.correction as Correction)
-                return {
-                    success: true,
-                    data: res.data?.message
-                }
-            } catch (err: any) {
-                if (err?.data?.errors) {
-                    if (err.data.errors.brief_id || err.data.errors.student_id || err.data.errors.message || err.data.errors.details) {
-                        let { brief_id, student_id, message, details } = err.data.errors;
-                        brief_id = brief_id ? brief_id[0] : "";
-                        student_id = student_id ? student_id[0] : "";
-                        message = message ? message[0] : "";
-                        details = details ? details[0] : "";
-                        return {
-                            success: false,
-                            errors: {
-                                brief_id,
-                                student_id,
-                                message,
-                                details
-                            },
-                        }
-                    }
-                }
-                return {
-                    success: false,
-                    errors: {
-                        brief_id: '',
-                        student_id: '',
-                        message: '',
-                        details: ''
-                    },
-                }
-            }
-        }
 
         return {
             corrections,
@@ -128,8 +79,6 @@ export const useCorrection = defineStore(
             fetchStudentCorrection,
             fetchStudentCorrectionsById,
             fetchCorrection,
-            createCorrection,
-            updateCorrection
         }
     },
     {
