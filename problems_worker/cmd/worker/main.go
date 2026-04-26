5ac300fcc73df49c2b94f62ac2b21028eda3433d
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-04-26 01:27:08 +0100

    feat: Load environment variables for Redis configuration

--- problems_worker/cmd/worker/main.go
+++ problems_worker/cmd/worker/main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
-	goredis "github.com/redis/go-redis/v9"
 	"problems_worker/internal/jobs"
-	myredis "problems_worker/internal/redis"       
+	myredis "problems_worker/internal/redis"
+
+	"github.com/joho/godotenv"
+	goredis "github.com/redis/go-redis/v9"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	redisHost := os.Getenv("REDIS_HOST")
+	redisPort := os.Getenv("REDIS_PORT")
+
 	rdb := goredis.NewClient(&goredis.Options{
-		Addr: "redis:6379",
+		Addr: fmt.Sprintf("%s:%s", redisHost, redisPort),
 	})
 
 	log.Println("Worker started...")
